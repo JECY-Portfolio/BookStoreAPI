036768a1c90372803c23ede06803b4896877dd62
AKINTOBA <samuelakinwole@gmail.com>
2025-08-12 22:36:16 +0100

    Add Entities to the BookStore Application

---
+++ Book-Store-API/Entites/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store_API.Entites;
+
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; } 
+    public decimal Price { get; set; }
+
+    //Foreign Properties
+    public int AuthorId { get; set; } 
+    public Author? Author { get; set; } 
+    public int CategoryId { get; set; } 
+    public Category? Category { get; set; } 
+}
