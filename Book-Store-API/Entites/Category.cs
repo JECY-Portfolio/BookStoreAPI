831c07a3f019f562334ea85cb2efdcd47a4d2ab3
Geoffrey Jecinta <jecintageoffrey@yahoo.com>
2025-08-17 17:08:33 +0100

    Add [JsonIgnore] to Category.Books to simplify POST request in Swagger

--- Book-Store-API/Entites/Category.cs
+++ Book-Store-API/Entites/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Book_Store_API.Entites;
+﻿using System.Text.Json.Serialization;
+
+namespace Book_Store_API.Entites;
 
 public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; } 
     public string? Description { get; set; }
+    [JsonIgnore]
     public ICollection<Book> Books { get; set; } = new List<Book>();
 }
